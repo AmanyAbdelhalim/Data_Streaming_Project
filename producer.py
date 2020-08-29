20f7fab5b2fdaa20ef06240c078423ecc5d156cc
Amany Abdelhalim <amany.abdelhalim@icloud.com>
2020-08-29 19:31:48 -0400

    update producer

--- producer.py
+++ producer.py
@@ -1,33 +1,3 @@
-# # Extends Bundle.ML Serialization for Pipelines
-# import mleap.sklearn.pipeline
-#
-# import numpy as np
-# from mleap.sklearn.logistic import LogisticRegression
-# node_name = "root"
-# #node_name = "{}.node".format("model.json")
-#
-# logistic_regression_tf = LogisticRegression()
-#
-# model = logistic_regression_tf.deserialize_from_bundle("/Users/amanyabdelhalim/Downloads/lr_model", node_name)
-# print(model)
-#
-# #input=[0, 110, [12, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 29, 30, 31, 32, 33, 34, 35, 36, 37, 59, 62, 89, 96], [5, 954, 4, 1, 5, 1, 1401, 1, 2, 450, 1, 3, 1, 3, 3, 3, 2, 3, 2, 1, 1, 1, 1, 1, 1]]
-# input=[0, 110, [12, 13, 15, 16, 17, 18, 20, 21, 23, 24, 27, 28, 30, 31, 34, 37, 59, 61, 87, 96], [5, 1, 1476, 4, 1, 5, 588, 1, 82, 1, 2, 3, 3, 3, 3, 1, 1, 1, 1, 1]]
-# def convertInput(input):
-#     output=[]
-#     input = input.replace("(","[").replace(")","]")
-
-    # input=json.loads(input)
-#     for i in range(110):
-#         output.append(0)
-#     ind=0
-#     for num in input[1]:
-#         output[num]=input[2][ind]
-#         ind +=1
-#     return output
-#
-# expected = model.predict(np.array(convertInput(input)).reshape(1, -1))
-# print(expected)
 
 from time import sleep
 from json import dumps
