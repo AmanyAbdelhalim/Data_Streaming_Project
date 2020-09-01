3496d6fba4b8f430acd8f09f4bf8dcf347062c7f
Amany Abdelhalim <amany.abdelhalim@icloud.com>
2020-08-31 20:55:23 -0400

    update

--- consumer.py
+++ consumer.py
@@ -7,80 +7,89 @@ from mleap.sklearn.logistic import LogisticRegression
 from mleap.sklearn.svm import LinearSVC
 
 import numpy as np
-node_name = "root"
-logistic_regression_tf = LogisticRegression()
-#random_forest = RandomForestClassifier()
-svm = LinearSVC()
-# creating the Kafka producer object
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
+class ConsumerProducer:
 
-#model_RF = random_forest
-model_LR = logistic_regression_tf.deserialize_from_bundle("/Users/amanyabdelhalim/Desktop/weCloudData/criteo/lr_model", node_name)
-model_LSVM = svm.deserialize_from_bundle("/Users/amanyabdelhalim/Desktop/weCloudData/criteo/lsvm_model", node_name)
+    def __init__(self,pHost,cHost,gId,lrModelPath,svmModelPath) :
+        self.node_name = "root"
+        # creating the Kafka producer object
+        self.producer = KafkaProducer(bootstrap_servers=[pHost],
+                                 value_serializer=lambda x:
+                                 dumps(x).encode('utf-8'))
+        # create a kafka consumer object
+        self.consumer = KafkaConsumer(
+            'ml',
+            bootstrap_servers=[cHost],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id=gId,
+            value_deserializer=lambda x: loads(x.decode('utf-8')))
+        self.logistic_regression_tf = LogisticRegression()
+        # random_forest = RandomForestClassifier()
+        self.svm = LinearSVC()
+        self.model_LR = self.logistic_regression_tf.deserialize_from_bundle(lrModelPath, self.node_name)
+        self.model_LSVM = self.svm.deserialize_from_bundle(svmModelPath, self.node_name)
 
-#print(model_LSVM)
+    # convert the sparse vector (features column) to dense vector
+    def convertToDenseVector(self,input):
+        output=[]
+        for i in range(input[0]):
+            output.append(0)
+        ind=0
+        for num in input[1]:
+            output[num]=input[2][ind]
+            ind +=1
+        return output
 
-# convert the sparse vector (features column) to dense vector
-def convertToDenseVector(input):
-    output=[]
-    for i in range(input[0]):
-        output.append(0)
-    ind=0
-    for num in input[1]:
-        output[num]=input[2][ind]
-        ind +=1
-    return output
-# create a kafka consumer object
-consumer = KafkaConsumer(
-    'ml',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     group_id='criteo-consumer-2',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-# for each message consumed, we will convert the features into dense vector,
-# predict the label using the ML model
-# check whether the prediction matches the original label (correct) or not (incorrect).
-# 1 represents correct = True and 0 represents correct = False.
-for message in consumer:
-    message = message.value
-    featureVector = convertToDenseVector(message['feature'])
-    predicted_lr = model_LR.predict(np.array(featureVector).reshape(1, -1))
-    predicted_svm = model_LSVM.predict(np.array(featureVector).reshape(1, -1))
-    if predicted_lr[0] == message['label']:
-        correct_lr = 1
-    else:
-        correct_lr = 0
+    # for each message consumed, we will convert the features into dense vector,
+    # predict the label using the ML model
+    # check whether the prediction matches the original label (correct) or not (incorrect).
+    # 1 represents correct = True and 0 represents correct = False.
+    def prepareConsumedMaessagesAndSend(self):
+        for message in self.consumer:
+            message = message.value
+            featureVector = self.convertToDenseVector(message['feature'])
+            predicted_lr = self.model_LR.predict(np.array(featureVector).reshape(1, -1))
+            predicted_svm = self.model_LSVM.predict(np.array(featureVector).reshape(1, -1))
+            if predicted_lr[0] == message['label']:
+                correct_lr = 1
+            else:
+                correct_lr = 0
 
-    if predicted_svm[0] == message['label']:
-        correct_svm = 1
-    else:
-        correct_svm = 0
+            if predicted_svm[0] == message['label']:
+                correct_svm = 1
+            else:
+                correct_svm = 0
 
-    print('Expected {}  -- > Got: {} with logistic regression model and correct {}'.format(message['label'],predicted_lr,correct_lr))
-    print('Expected {}  -- > Got: {} with random forest model and correct {}'.format(message['label'],predicted_svm,correct_svm))
+            print('Expected {}  -- > Got: {} with logistic regression model and correct {}'.format(message['label'],predicted_lr,correct_lr))
+            print('Expected {}  -- > Got: {} with random forest model and correct {}'.format(message['label'],predicted_svm,correct_svm))
 
-    # preparing the two dictionaries that will be sent to kafka, one for each model
-    # logistic regression dictionary
-    lr_d = dict()
-    lr_d["model"] = "lr"
-    lr_d["prediction"] = str(predicted_lr[0])
-    lr_d["label"] = message['label']
-    # adding correct value to the dictionaries
-    lr_d["correct"] = correct_lr
+            # preparing the two dictionaries that will be sent to kafka, one for each model
+            # logistic regression dictionary
+            lr_d = dict()
+            lr_d["model"] = "lr"
+            lr_d["prediction"] = str(predicted_lr[0])
+            lr_d["label"] = message['label']
+            # adding correct value to the dictionaries
+            lr_d["correct"] = correct_lr
 
-    # SVM dictionary
-    svm_d = dict()
-    svm_d["model"] = "svm"
-    svm_d["prediction"] = str(predicted_svm[0])
-    svm_d["label"] = message['label']
-    # adding correct value to the dictionaries
-    svm_d["correct"] = correct_svm
+            # SVM dictionary
+            svm_d = dict()
+            svm_d["model"] = "svm"
+            svm_d["prediction"] = str(predicted_svm[0])
+            svm_d["label"] = message['label']
+            # adding correct value to the dictionaries
+            svm_d["correct"] = correct_svm
 
-    # send the prediction, label and correct to kafka again to be consumed by logstash
-    producer.send('predictions', value=lr_d)
-    producer.send('predictions', value=svm_d)
+            # send the prediction, label and correct to kafka again to be consumed by logstash
+            self.producer.send('predictions', value=lr_d)
+            self.producer.send('predictions', value=svm_d)
 
+if __name__=="__main__":
+    lrModelPath="/Users/amanyabdelhalim/Desktop/weCloudData/criteo/lr_model"
+    svmModelPath="/Users/amanyabdelhalim/Desktop/weCloudData/criteo/lsvm_model"
+    pHost = 'localhost:9092'
+    cHost = 'localhost:9092'
+    groupId = 'criteo-consumer-2'
+    pC = ConsumerProducer(pHost, cHost, groupId, lrModelPath, svmModelPath)
+    pC.prepareConsumedMaessagesAndSend()
--- producer.py
+++ producer.py
@@ -5,36 +5,48 @@ from kafka import KafkaProducer
 import json
 import pyarrow.parquet as pq
 
-# read the test data(label, features) stored as a parquet file object
-parquet_file = pq.ParquetFile('/Users/amanyabdelhalim/Desktop/weCloudData/criteo/feature_label_only.parquet')
+class Producer:
+    def __init__(self, testDataPath,host):
+        # read the test data(label, features) stored as a parquet file object
+        self.parquet_file = pq.ParquetFile(testDataPath)
+        print('-----------------------')
+        # creating the Kafka producer object
+        self.producer = KafkaProducer(bootstrap_servers=[host],
+                                 value_serializer=lambda x:
+                                 dumps(x).encode('utf-8'))
 
-print('-----------------------')
-# creating the Kafka producer object
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
-# read the two attributes and put them in a table
-table = parquet_file.read(columns=["label","features1"],use_threads=True)
-# column 0 is the label
-label = table.columns[0]
-# column 1 is the features
-features = table.columns[1]
+    def prepareAndSendToKafka(self):
+        # read the two attributes and put them in a table
+        table = self.parquet_file.read(columns=["label", "features1"], use_threads=True)
+        # column 0 is the label
+        label = table.columns[0]
+        # column 1 is the features
+        features = table.columns[1]
 
-# creating an empty dictionary
-d = dict()
-for i in range(len(label)):
-    l=label[i].as_py() # converting the arrow scalar types to python types
-    f=features[i].as_py() # converting the arrow scalar types to python types
-    # formatting the string to look like json string
-    featureStr = f.replace("(", "[").replace(")", "]")
-    # converts the json array string to list object
-    featureList = json.loads(featureStr)
-    d["label"]=l
-    d["feature"]= featureList
-    print(d)# printing the dictionary that has the label and the feature
-    # send the message (dictionary with the label and features) to Kafka
-    producer.send('ml', value=d)
-    sleep(3)
+        # creating an empty dictionary that will hold the message that we will send to Kafka
+        d = dict()
+        for i in range(len(label)):
+            l = label[i].as_py()  # converting the arrow scalar types to python types
+            f = features[i].as_py()  # converting the arrow scalar types to python types
+            # formatting the string to look like json string
+            featureStr = f.replace("(", "[").replace(")", "]")
+            # converts the json array string to list object
+            featureList = json.loads(featureStr)
+            d["label"] = l
+            d["feature"] = featureList
+            print(d)  # printing the dictionary that has the label and the feature
+            # send the message (dictionary with the label and features) to Kafka
+            self.producer.send('ml', value=d)
+            sleep(3)
+
+
+
+
+if __name__=="__main__":
+    TestDataPath='/Users/amanyabdelhalim/Desktop/weCloudData/criteo/feature_label_only.parquet'
+    host = 'localhost:9092'
+    p = Producer(TestDataPath, host)
+    p.prepareAndSendToKafka()
 
 
 
